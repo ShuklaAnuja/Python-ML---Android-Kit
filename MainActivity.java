d2e3f3e909d78b44960f01e1415093f2f1be6eab
Anuja Shukla <anujashukla1993@gmail.com>
2021-04-09 10:06:37 +0530

    Android code update

---
+++ MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.myfirstmlandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetFileDescriptor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button button;
+    Interpreter interpreter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.inputX);
+        button = findViewById(R.id.predictbtn);
+        textView = findViewById(R.id.outputY);
+
+        try {
+            interpreter = new Interpreter(loadModelFile(),null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+               float f =  doInference(editText.getText().toString());
+               textView.setText("Result - "+f);
+
+            }
+        });
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException
+    {
+        AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("linear.tflite");
+        FileInputStream fileInputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long len = assetFileDescriptor.getLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,len);
+    }
+
+    public float doInference(String val)
+    {
+        float[] input = new float[1];
+        input[0] = Float.parseFloat(val);
+        float[][] output = new float[1][1];
+
+        interpreter.run(input,output);
+        return  output[0][0];
+    }
+}
